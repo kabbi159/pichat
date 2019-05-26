4508e9fcd9661dd4282e5c51e070d60b743850a1
Jiwung Hyun <kabbi159@gmail.com>
2019-05-27 02:35:06 +0900

    이미지 데이터 받아서 저장 및 얼굴 인식 부분 코드 완성 (주의 - 본인 얼굴 없음)

--- webserver.py
+++ webserver.py
@@ -3,6 +3,7 @@ import requests
 import json
 import urllib.request
 from urllib.parse import unquote
+from binascii import a2b_base64
 
 import face_recognition
 from PIL import Image, ImageDraw
@@ -27,36 +28,90 @@ def preprocess_friend_information():
     # request로 전해진 데이터를 받는다. (현재 받는 데이터는 친구 API의 결과)
     friend_info = request.get_data()
 
+    print(friend_info)
+
     # 받은 데이터가 binary string 및 URI가 UTF-8로 인코딩이 되어있으므로, decode 및 unquote를 해준다.
     friend_info = friend_info.decode('UTF-8')
     friend_info = unquote(friend_info)
-    print(friend_info)
+    # print(friend_info)
 
     # &로 구분하여 리스트에 넣는다
     a = friend_info.split('&')  # 맨 뒤에서 3번째 까지 total_count, after_url, result_id
     # 한 친구마다 elements[i][profile_nickname], elements[i][profile_thumbnail_image], elements[i][id] 가 존재함.
 
+    # for i in a:
+    #     print(i)
+
+    dataURI = a[0][6:]
+    print(dataURI)
+    head, data = dataURI.split(',', 1)
+    print(data)
+
+    binary_data = a2b_base64(data)
+    print(binary_data)
+
+    fd = open('unknown.jpg', 'wb')
+    fd.write(binary_data)
+    fd.close()
+
+
     # 친구 숫자 구하는 코드
-    total_count = int(a[-3][12:])
+    total_count = int(a[-3][19:])
     print(total_count)
 
     # 친구 숫자 만큼 친구 정보를 추출한다. (이름, 이미지 저장, id)
     for i in range(total_count):
-        profile_nickname = a[3*i].split('=')[1]
-        profile_thumbnail_image = a[3*i + 1].split('=')[1]
+        profile_nickname = a[3*i+1].split('=')[1]
+        profile_thumbnail_image = a[3*i + 2].split('=')[1]
         urllib.request.urlretrieve(profile_thumbnail_image, 'known/'+profile_nickname+'.jpg')
-        id = a[3*i + 2].split('=')[1]
+        id = a[3*i + 3].split('=')[1]
         print(profile_nickname, profile_thumbnail_image, id)
 
     # face recognition API 사용을 위하여, 친구 이미지들이 저장되어 있는 디렉토리로부터 사진을 가져온 다음, load & encode 한다.
     friend_img_path_list = os.listdir('known')
     print(friend_img_path_list)
     friend_img_list = []
+    friend_name_list = []
     for img in friend_img_path_list:
         img_loaded = face_recognition.load_image_file('known/' + img)
-        img_encoded = face_recognition.face_encodings(img_loaded)
+        friend_name_list.append(img.split('.')[0])
+        img_encoded = face_recognition.face_encodings(img_loaded)[0]
         # print(len(img_encoded))
         friend_img_list.append(img_encoded)
+
+    unknown_img = face_recognition.load_image_file('unknown.jpg')
+
+    face_locations = face_recognition.face_locations(unknown_img)
+    face_encodings = face_recognition.face_encodings(unknown_img, face_locations)
+
+    pil_image = Image.fromarray(unknown_img)
+    draw = ImageDraw.Draw(pil_image)
+
+    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+        # See if the face is a match for the known face(s)
+        matches = face_recognition.compare_faces(friend_img_list, face_encoding)
+
+        name = "Unknown"
+
+        # Or instead, use the known face with the smallest distance to the new face
+        face_distances = face_recognition.face_distance(friend_img_list, face_encoding)
+        best_match_index = np.argmin(face_distances)
+        if matches[best_match_index]:
+            name = friend_name_list[best_match_index]
+            print(name)
+            name = str(best_match_index)
+
+        # Draw a box around the face using the Pillow module
+        draw.rectangle(((left, top), (right, bottom)), outline=(0, 0, 255))
+
+        # Draw a label with a name below the face
+        text_width, text_height = draw.textsize(name)
+        draw.rectangle(((left, bottom - text_height - 10), (right, bottom)), fill=(0, 0, 255), outline=(0, 0, 255))
+        draw.text((left + 6, bottom - text_height - 5), name, fill=(255, 255, 255, 255))
+
+    del draw
+
+    pil_image.show()
 
     return render_template('test.html')
 
